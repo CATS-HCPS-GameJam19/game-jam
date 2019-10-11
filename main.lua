7324ed00c848ccce926585e5f64bc7f944b4e6c0
Carlo <CarloMehegan@gmail.com>
2019-10-11 11:10:23 -0400

    rover continuously moves

--- main.lua
+++ main.lua
@@ -8,7 +8,18 @@ function love.load()
 end
 
 function love.update(dt)
-
+  if love.keyboard.isDown("d") then
+    rover.x = rover.x + 10
+  end
+  if love.keyboard.isDown("s") then
+    rover.y = rover.y + 10
+  end
+  if love.keyboard.isDown("a") then
+    rover.x = rover.x - 10
+  end
+  if love.keyboard.isDown("w") then
+    rover.y = rover.y - 10
+  end
 end
 
 function love.draw()
@@ -16,44 +27,40 @@ function love.draw()
 end
 
 
-function FindProximity(x,y)
-  x0 = (x - boy.x)^2
-  y0 = (y - boy.y)^2
-  return math.sqrt(x0+y0)
-end
-
-function Move(key)
-
-  if key == "d" then
-    rover.x = rover.x + 10
-  end
-  if key == "s" then
-    rover.y = rover.y + 10
-  end
-  if key == "a" then
-    rover.x = rover.x - 10
-  end
-  if key == "w" then
-    rover.y = rover.y-1
-  end
-
-end
 
 
-function love.keypressed(key, scancode, isrepeat)
-  if key == "escape" then
-    love.event.quit()
-  end
-  if key == "d" then
-    Move("d")
-  end
-  if key == "s" then
-    Move("s")
-  end
-  if key == "a" then
-    Move("a")
-  end
-  if key == "w" then
-    Move("w")
-  end
-end
+-- function Move(key)
+--
+--   if key == "d" then
+--     rover.x = rover.x + 10
+--   end
+--   if key == "s" then
+--     rover.y = rover.y + 10
+--   end
+--   if key == "a" then
+--     rover.x = rover.x - 10
+--   end
+--   if key == "w" then
+--     rover.y = rover.y - 10
+--   end
+--
+-- end
+
+
+-- function love.keypressed(key, scancode, isrepeat)
+--   if key == "escape" then
+--     love.event.quit()
+--   end
+--   if key == "d" then
+--     Move("d")
+--   end
+--   if key == "s" then
+--     Move("s")
+--   end
+--   if key == "a" then
+--     Move("a")
+--   end
+--   if key == "w" then
+--     Move("w")
+--   end
+-- end
