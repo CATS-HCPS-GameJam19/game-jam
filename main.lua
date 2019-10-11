17922e674222c4c9a715c1bc84cc5e8594c9a493
Carlo <CarloMehegan@gmail.com>
2019-10-10 23:00:06 -0400

    added main and middleclass files

---
+++ main.lua
@@ -0,0 +1,16 @@
+-- 2019 HCPSxJMU Game Jam
+-- Carlo Mehegan, Austin Spitzer, Thomas Shulgan, Stella Alexiou
+-- October 11, 2019
+
+
+function love.load(arg)
+  -- body...
+end
+
+function love.update(dt)
+  -- body...
+end
+
+function love.draw()
+  -- body...
+end
