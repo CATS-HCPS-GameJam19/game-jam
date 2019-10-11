3d722e9ef4a6b210c19bc91e091adda396302c23
Carlo <CarloMehegan@gmail.com>
2019-10-11 14:23:29 -0400

    tweaked sizes of tiles and rover and cleaned code

--- rover.lua
+++ rover.lua
@@ -5,8 +5,9 @@ Rover = class('Rover')
 function Rover:initialize(x, y)
   self.x = x
   self.y = y
-  self.w = 8
-  self.h = 8
+  self.s = 2.5
+  self.w = 32 * self.s
+  self.h = 32 * self.s
   self.img = love.graphics.newImage('sprites/Curiosity Mars.png')
 end
 
@@ -16,7 +17,7 @@ end
 
 function Rover:draw()
   love.graphics.setColor(1,1,1)
-  love.graphics.draw(self.img, (self.x), (self.y), 0, 2)
+  love.graphics.draw(self.img, (self.x), (self.y), 0, self.s)
 end
 
 return Rover
