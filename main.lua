af32b87cf519e4ec7398c4d3571dcda6cb1bc406
austinjs471 <56421736+austinjs471@users.noreply.github.com>
2019-10-12 11:21:56 -0400

    Fixed merge issues

--- main.lua
+++ main.lua
@@ -10,23 +10,15 @@ local Battery = require 'battery'
 
 
 function love.load()
-<<<<<<< HEAD
-  Mars = love.graphics.newImage("Sprites/Mars Background.png")
-=======
   mineral = Mineral:new(100, 100);
   m2 = Mineral:new(50, 50)
->>>>>>> 5bda14713188da6125c674aa052f6125260e8048
   rover = Rover:new(love.graphics.getWidth() / 2, love.graphics.getHeight() / 2)
   map = Map:new(20,20,50)
   battery = Battery:new()
 end
 
 function love.update(dt)
-<<<<<<< HEAD
-
-=======
   battery:update(-0.05)
->>>>>>> 5bda14713188da6125c674aa052f6125260e8048
   if love.keyboard.isDown("d") then
     rover.r = rover.r + .08
   end
@@ -43,17 +35,12 @@ function love.update(dt)
     rover.x = rover.x + math.cos(rover.r - math.pi/2) * 1.5
     battery:update(-0.25)
   end
-<<<<<<< HEAD
- camera.x = (rover.x + (rover.w/2)) - (love.graphics.getWidth()/2)
- camera.y = (rover.y + (rover.h/2)) - (love.graphics.getHeight()/2)
- camera:checkBorderCollision(map)
- rover:update(dt,map)
-=======
   battery.x = rover.x  - (love.graphics.getWidth()/2) + 30
   battery.y = rover.y  - (love.graphics.getHeight()/2) + 30
   camera.x = rover.x  - (love.graphics.getWidth()/2)
   camera.y = rover.y  - (love.graphics.getHeight()/2)
->>>>>>> 5bda14713188da6125c674aa052f6125260e8048
+  camera:checkBorderCollision(map)
+  rover:update(dt,map)
 end
 
 
